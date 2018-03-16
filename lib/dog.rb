d26c18b37d3d33f226b608950642796d3501b4ae
amanda johnson <manda.panda91@yahoo.com>
2018-03-16 16:36:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,6 +59,8 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else 
       
     end
   end
